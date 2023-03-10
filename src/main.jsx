bb77f98148b9259e72e59724c941540e516a6a6b
pengyw97 <pengyw97@gmail.com>
2023-03-10 11:13:53 +0800

    feat: 引入路由

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello vite</div>,
+  },
+  {
+    path: '/about',
+    element: <div>Hello about</div>,
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>Hello vite</React.StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
